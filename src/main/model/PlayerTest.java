d2d56ce7e714bb786411996460d1d3da8804840c
user.name <antoniolozanolarrubia@gmail.com>
2024-10-14 20:33:35 +0200

    testPlayer

---
+++ src/main/model/PlayerTest.java
@@ -0,0 +1,14 @@
+package main.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
