2ade96b420ec583a5c02181bb195d7dd098f550e
Eloi Milego <eloimilego@gmail.com>
2024-10-27 21:32:47 +0100

    first implementation knight

--- src/main/java/model/Knight.java
+++ src/main/java/model/Knight.java
@@ -8,6 +8,28 @@ public class Knight extends Piece {
 
     @Override
     public boolean validMovement(Square destination, Board board) {
+        if (destination == null) {
+            return false;
+        }
+
+        // Invariant check for board bounds
+        if ((destination.getRow() < 0 || destination.getRow() >= board.getSize()) ||
+            (destination.getColumn() < 0 || destination.getColumn() >= board.getSize())) {
+            return false;
+        }
+
+        int rowDelta = Math.abs(destination.getRow() - this.position.getRow());
+        int colDelta = Math.abs(destination.getColumn() - this.position.getColumn());
+
+        // Validate "L" shape movement
+        if ((rowDelta == 2 && colDelta == 1) || (rowDelta == 1 && colDelta == 2)) {
+            Piece destinationPiece = destination.getPiece();
+
+            // TODO: check same color
+            
+            return checkInvariants();
+        }
+
         return false;
     }
 
