67aa0f302ed3a7b8529741b2abd2d7777cb9599b
user.name <antoniolozanolarrubia@gmail.com>
2024-10-28 18:40:41 +0100

    King class

---
+++ src/main/java/model/King.java
@@ -0,0 +1,44 @@
+package model;
+
+public class King extends Piece {
+
+    public King(Color color) {
+        super(color);
+    }
+
+    @Override
+    public boolean validMovement(Square destination, Board board) {
+        if (destination == null) {
+            return false;
+        }
+
+        if (destination.getRow() < 0 || destination.getRow() >= board.getSize() &&
+            destination.getColumn() < 0 || destination.getColumn() >= board.getSize()) {
+            return false;
+        }
+
+        int rowDelta = Math.abs(destination.getRow() - this.position.getRow());
+        int colDelta = Math.abs(destination.getColumn() - this.position.getColumn());
+
+        // Check that movement is one square in any direction
+        if ((rowDelta <= 1 && colDelta <= 1) && !(rowDelta == 0 && colDelta == 0)) {
+            Piece destinationPiece = destination.getPiece();
+
+            // square occupied?
+            if (destinationPiece != null && destinationPiece.getColor() == this.color) {
+                return false;
+            }
+
+            if (!checkInvariants()) {
+                return false;
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkInvariants() {
+        return this.color != null;
+    }
+}
