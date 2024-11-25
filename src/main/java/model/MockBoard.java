e8c99f3357532311745ca9fbd37bd43d5a88ae95
user.name <antoniolozanolarrubia@gmail.com>
2024-11-25 17:51:23 +0100

    Manual Mock Board

--- src/main/java/model/MockBoard.java
+++ src/main/java/model/MockBoard.java
@@ -1,61 +1,30 @@
 package model;
 
 public class MockBoard extends Board {
-
-    private int mockSizeRows = 8;
-    private int mockSizeCols = 8;
-    private Square[][] mockSquares;
-
-    // //Simulate Board
-    public MockBoard() {
-        mockSquares = new Square[mockSizeRows][mockSizeCols];
-        initializeMockBoard();
-    }
-
-    // Init Mock board
-    private void initializeMockBoard() {
-        for (int row = 0; row < mockSizeRows; row++) {
-            for (int column = 0; column < mockSizeCols; column++) {
-                mockSquares[row][column] = new Square(row, column);
-            }
-        }
-    }
-
-    //
-    public void setMockSize(int rows, int cols) {
-        mockSizeRows = rows;
-        mockSizeCols = cols;
-        mockSquares = new Square[rows][cols];
-        initializeMockBoard();
-    }
-
-    @Override
-    public int getSizeRows() {
-        return mockSizeRows;
-    }
-
-    @Override
-    public int getSizeCols() {
-        return mockSizeCols;
-    }
-
-    @Override
-    public Square getSquare(int row, int column) {
-        if (row < 0 || row >= mockSizeRows || column < 0 || column >= mockSizeCols) {
-            return null; // out of bound
-        }
-        return mockSquares[row][column];
-    }
-
-    // Add piece in Mock board
-    public void setMockPieceAt(int row, int column, Piece piece) {
-        if (row >= 0 && row < mockSizeRows && column >= 0 && column < mockSizeCols) {
-            mockSquares[row][column].setPiece(piece);
-        }
-    }
+    private boolean hasKingWhite = true;
+    private boolean hasKingBlack = true;
 
     @Override
     public boolean hasKing(Color color) {
-        return true; 
+        if (color == Color.WHITE) {
+            return hasKingWhite;
+        } else {
+            return hasKingBlack;
+        }
     }
+
+    public void setKingPresence(Color color, boolean presence) {
+        if (color == Color.WHITE) {
+            hasKingWhite = presence;
+        } else {
+            hasKingBlack = presence;
+        }
+    }
+
+    @Override
+    public boolean movePiece(Square origin, Square destination) {
+        return true; // All valid movements
+    }
+
+ 
 }
